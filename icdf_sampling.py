25619b6e3e6058c2dcc3a5286d93f317e1810897
modichirag <modichirag@berkeley.edu>
2018-07-12 15:22:23 +0200

    assign galaxy mass

--- icdf_sampling.py
+++ icdf_sampling.py
@@ -1,4 +1,8 @@
-def icdf_sampling(bs, mf = None, mv = None, mfv = None, match_high = True, hmass = None, M0 = None, N0 = None):
+import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline as interpolate
+from scipy.integrate import romberg
+
+def icdf_sampling(bs, mf = None, mv = None, mfv = None, match_high = True, hmass = None, M0 = None, N0 = None, seed=100):
     '''
     Given samples from analytic mass function (dN/dln(M)), find halo masss by matching abundance via
     inverse cdf sampling. 
@@ -55,7 +59,150 @@ def icdf_sampling(bs, mf = None, mv = None, mfv = None, match_high = True, hmass
     icdf = interpolate(cdf[:], 0.5*(lmm2[:-1] + lmm2[1:]))
     
     #Sample random points from uniform distribution and find corresponding mass
+    np.random.seed(seed)
     ran = np.random.uniform(0, 1, N0)
     hmatch = np.exp(icdf(ran))
     hmatch.sort()
     return hmatch[::-1]
+
+
+#######
+#For galaxies
+
+
+def csmfcen(m, mh=None, mstarc=1e12, sigc=0.212):
+    '''Eq. 3 in Reddick et.al. 10.1088/0004-637X/771/1/30'''
+
+    if mh is not None:
+        mstarc = mstarcen(mh)
+    fac = 1/np.sqrt(2*np.pi*sigc**2)
+    expf = -(np.log10(m) - np.log10(mstarc))**2/(2*sigc**2)
+    return fac * np.exp(expf)
+    
+
+    
+def csmfsat(m, mh=None, mstars=1e12, alpha=-1, phis=1):
+    '''Eq. 4 in Reddick et.al. 10.1088/0004-637X/771/1/30'''
+    if mh is not None:
+        mstars = mstarsat(mh)
+        phis = phistar(mh)
+    frac = m/mstars
+    return phis * frac ** (alpha+1) * np.exp(-frac)
+
+
+def mstarcen(mh):
+    '''Eq. 5 and table 4, VAGC values from Reddick et.al'''
+    m0 = 10**10.64
+    m1 = 10**12.59
+    g1 = 0.726
+    g2 = 0.065
+    logm = np.log10(m0) + g1*np.log10(mh/m1) + (g2-g1)*np.log10(1+mh/m1)
+    return 10**logm
+
+
+def phistar(mh):
+    '''Eq. 6 and table 4, VAGC values from Reddick et.al'''
+    mphi = 10**12.30
+    a = 0.866
+    return (mh/mphi)**a
+
+
+def mstarsat(mh):
+    '''Eq. 7 and table 4, VAGC values from Reddick et.al'''
+    m0 = 10**10.401
+    m1 = 10**12.71
+    b = 0.753
+    logm = np.log10(m0) + b*np.log10(mh/m1) - b*np.log10(1+mh/m1)
+    return 10**logm
+
+
+
+def createicdf(pdf=None, mv=None, mfv=None, Mthresh=10**10.5, M0=1e13):
+    '''For the galaxies, based on the functions from Reddick et.al above
+    Create a cdf for a halo mass and then sample from ssampleicdf
+    '''
+
+    Nint = 500 #No.of points to interpolate
+    if pdf is None:
+        if mfv is None:
+            print('need pdf or the values to interpolate over')
+            return None
+        else:
+            imf = interpolate(mv, mfv, k = 5)
+    else: imf = pdf
+    ilmf = lambda x: imf(10**(x))
+    lmm = np.linspace(np.log10(M0), np.log10(Mthresh), Nint)
+      
+    #Calculate the other mass-limit M2 of the catalog by integrating mf and comparing total number of halos
+    nbin = abs(np.array([romberg(ilmf, lmm[i], lmm[i+1]) for i in range(lmm.size-1)]))
+    ncum = np.cumsum(nbin)
+    ncum /= ncum.max()
+    icdf = interpolate(ncum, 0.5*(lmm[:-1] + lmm[1:]))    
+    return icdf
+
+def sampleicdf(icdf, N0, seed=11):
+    np.random.seed(seed)
+    ran = np.random.uniform(0, 1, N0)
+    hmatch = 10**(icdf(ran))
+    hmatch.sort()
+    return hmatch[::-1]
+
+        
+
+
+def assigngalaxymass(galcat, hmass, mbins=None, verbose=False, seed=100, Mthresh=10**10.5, M0=1e13):
+    '''
+    Assign central and satellite masses based on matching the conditional stellar mass
+    function from reddick et. al.
+    For halos in given mass bins (mbins), generate csmf and then match it by inverse
+    cdf sampling
+    '''
+    galtype = galcat['gal_type'].compute()
+    haloid = galcat['halo_id'].compute()
+    if verbose:
+        haloidcen = set(haloid[galtype==0])
+        haloidsat = set(haloid[galtype==1])
+        satonly = haloidsat - haloidcen
+        print('Number of halos with only satellites and no centrals = ', len(satonly))
+    
+    #hngal = np.zeros_like(hmass).astype('int')
+    #unique, counts = np.unique(haloid, return_counts=True)
+    #hngal[unique.astype('int')] = counts
+
+    if mbins is None:
+        mbins  = np.logspace(np.log10(hmass.min()), np.log10(hmass.max()), 10)[::-1]
+
+    galmass = np.zeros_like(galtype).astype('float64')
+
+    #Loop
+    for i in range(len(mbins)-1):
+        mh = mbins[i] + mbins[i+1]
+        mh /= 2
+        try: r1, r2 = np.where(hmass < mbins[i])[0][0], np.where(hmass <= mbins[i+1])[0][0]
+        except: r1, r2 = np.where(hmass < mbins[i])[0][0], hmass.size
+        if verbose: print("For halos in rank : ", r1, r2)
+
+        mask = (haloid >= r1) & (haloid < r2)
+        cens = mask & (galtype == 0)
+        sats = mask & (galtype == 1)
+
+        if verbose:
+            ngal = hngal[r1:r2].sum()
+            print('mask sum, #cen, #sat, #gal = ', mask.sum(), cens.sum(), sats.sum(), ngal)
+            if ngal != cens.sum() + sats.sum():
+                print('wtf, ngal != cen+sat')
+
+
+        mm = np.logspace(8, np.log10(mh))
+
+        #mfv = csmfcen(mm, mh = mh)
+        #icdfc = sampler(M0=mh, mv=mm, mfv=mfv)
+        icdfc = createicdf(pdf = lambda x: csmfcen(x, mh=mh), Mthresh=Mthresh, M0=mh)
+        galmass[cens] = sampleicdf(icdfc, cens.sum(), seed=seed*i)
+
+        mfv = csmfsat(mm, mh = mh)
+        icdfs = createicdf(mv=mm, mfv=mfv, Mthresh=Mthresh, M0=mh)
+    #     icdfs = sampler(pdf = lambda x: icdf.csmfsf(x, mh=mh), M0=mh)
+        galmass[sats] = sampleicdf(icdfs, sats.sum(), seed=seed*i)
+
+    return galmass
